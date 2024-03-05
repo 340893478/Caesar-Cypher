e2f9f58cf7fb7058fdedf10c458cd8343813845b
340893478 <162224872+340893478@users.noreply.github.com>
2024-03-04 19:05:15 -0500

    Almost solved

--- caesar.py
+++ caesar.py
@@ -4,18 +4,19 @@ def caesar_encrypt(word, shiftInterval):
 
     new_text = ""
 
-    count = 0
-
     # Make it so that when letter is h then it shifts 3 interval to k and then this cycle repeats to all the letters shifting other 3 words aka h --> k
     for i in range(len(word)):
         # if word[i] == alphabet[i]:
         # new_text += alphabet[i + shiftInterval]
         for j in range(len(alphabet)):
             if word[i] == alphabet[j]:
-                swap = alphabet[i]
-                new_text += alphabet[i + 3]
+                swap = j + int(shift_interval)
+                new_text += alphabet[swap]
                 # new_text += alphabet[j]
     return new_text
 
-caesar = caesar_encrypt("hello", 3)
+word = input("Enter word: ")
+shift_interval = input("Enter shift interval: ")
+
+caesar = caesar_encrypt(word, shift_interval)
 print(caesar)
