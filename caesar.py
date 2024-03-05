f154a2e0326220939b7cbe971e6fbdfc420a901d
340893478 <162224872+340893478@users.noreply.github.com>
2024-03-04 21:27:58 -0500

    FINAL

--- caesar.py
+++ caesar.py
@@ -1,5 +1,5 @@
 # Johnny Huang (340893478)
-def caesar_encrypt(word, shiftInterval):
+def caesar_encrypt(word, shift_interval):
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     new_text = ""
